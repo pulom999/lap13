f5fa14eb85e00b2292065364714bdd2fc31a6d32
pulom999 <pulom999@gmail.com>
2021-12-24 15:42:33 +0700

    อัพครั้งที่ 2

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -5,8 +5,12 @@ using namespace std;
 long long int fibonacci(long long int x);
 
 int main(){
-    cout << fibonacci(50);
-    return 0; 
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
 }
 
 long long int fibonacci(long long int x){
