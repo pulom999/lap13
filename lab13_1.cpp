63ea8397e33a06d080014e06302534784a2c7a6c
pulom999 <pulom999@gmail.com>
2021-12-24 15:41:39 +0700

    อัพครั้งที่ 1

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,18 @@
 #include<iostream>
+#include <ctime>
 using namespace std;
 
+long long int fibonacci(long long int x);
+
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
+    cout << fibonacci(50);
     return 0; 
+}
+
+long long int fibonacci(long long int x){
+    if(x<=1){
+        return x;
+    }else{
+        return fibonacci(x-1)+fibonacci(x-2);
+    }
 }
